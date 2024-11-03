06283e309edb671f202aaf1b4e3fd3fc696e99ad
NoeliaRio <noe.lucia.rio@gmail.com>
2024-11-02 23:23:45 -0300

    [ADD]Se creo el controlador y vistas de cartas

---
+++ HandHunters/Controllers/CartasController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using HandHunters.Data;
+using HandHunters.Models;
+
+namespace HandHunters.Controllers
+{
+    public class CartasController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CartasController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Cartas
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.cartas.ToListAsync());
+        }
+
+        // GET: Cartas/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var carta = await _context.cartas
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (carta == null)
+            {
+                return NotFound();
+            }
+
+            return View(carta);
+        }
+
+        // GET: Cartas/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Cartas/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,NombreCarta,Descripcion,Costo,IdCalidad,IdEstado,IdIdioma,Foto,IdFranquicia,IdUser")] Carta carta)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(carta);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(carta);
+        }
+
+        // GET: Cartas/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var carta = await _context.cartas.FindAsync(id);
+            if (carta == null)
+            {
+                return NotFound();
+            }
+            return View(carta);
+        }
+
+        // POST: Cartas/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,NombreCarta,Descripcion,Costo,IdCalidad,IdEstado,IdIdioma,Foto,IdFranquicia,IdUser")] Carta carta)
+        {
+            if (id != carta.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(carta);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CartaExists(carta.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(carta);
+        }
+
+        // GET: Cartas/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var carta = await _context.cartas
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (carta == null)
+            {
+                return NotFound();
+            }
+
+            return View(carta);
+        }
+
+        // POST: Cartas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var carta = await _context.cartas.FindAsync(id);
+            if (carta != null)
+            {
+                _context.cartas.Remove(carta);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CartaExists(int id)
+        {
+            return _context.cartas.Any(e => e.Id == id);
+        }
+    }
+}
